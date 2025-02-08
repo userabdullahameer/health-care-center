52f596d10932c8becbe42e1a0d9cee723bf2a232
userabdullahameer <itabdullahameer@gmail.com>
2025-02-08 15:47:24 +0500

    Add files via upload

---
+++ tailwind.config.mjs
@@ -0,0 +1,21 @@
+import flowbite from  'flowbite-react/tailwind';
+
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    flowbite.content(),
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+    },
+  },
+  plugins: [flowbite.plugin()],
+};
